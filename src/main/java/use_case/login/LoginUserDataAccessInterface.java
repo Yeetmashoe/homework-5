69b288120a20270aa3b87644079c3e09852c3e58
Matthew <matthewchen@matthewsair106.home>
2025-07-03 13:39:05 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the username of the currently logged-in user.
+     * @param name the username of the user who has just logged in
+     */
+    void setCurrentUser(String name);
+
+    /**
+     * Returns the username of the currently logged-in user.
+     * @return the username of the logged-in user, or null if no user is logged in
+     */
+    String getCurrentUser();
 }
